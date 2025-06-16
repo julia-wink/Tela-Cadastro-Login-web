088a0262eb153522ec40a2914e9652f4f67f088d
julia-wink <109316266+julia-wink@users.noreply.github.com>
2025-06-16 19:27:25 -0300

    Create db_config.php

---
+++ db_config.php
@@ -0,0 +1,8 @@
+<?php
+    // Arquivo de configuração do Banco de Dados
+
+    define('DB_SERVER', '127.0.0.1'); // Geralmente 'localhost' ou '127.0.0.1'
+    define('DB_USERNAME', ' ');   // Seu usuário do MySQL/MariaDB
+    define('DB_PASSWORD', '');     // Sua senha do MySQL/MariaDB (vazia para XAMPP/WAMP padrão)
+    define('DB_NAME', 'bdteste');  // Nome do banco de dados que você criou
+    ?>
